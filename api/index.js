88405cfb47126bc563c19b99f5da986740db9cab
WrecKunai <escape4ome@gmail.com>
2023-05-14 13:44:37 +0530

    final

--- api/index.js
+++ api/index.js
@@ -50,7 +50,7 @@ const server = app.listen(PORT, () => {
 });
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
   },
 });
 let users = [];
